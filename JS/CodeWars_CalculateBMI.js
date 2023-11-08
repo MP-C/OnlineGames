30551c43f7d938213acee4af3aba739e24cd4504
MP-C <mp.crcarvalho@gmail.com>
2023-11-08 23:53:24 +0100

    CodeWars_CalculateBMI.js

--- JS/CodeWars_CalculateBMI.js
+++ JS/CodeWars_CalculateBMI.js
@@ -9,3 +9,18 @@ function bmi(weight, height) {
         return "Obese";
     };
 }
+
+function bmi(weight, height) {
+    const bmi = weight / (height * height)
+    if(bmi <= 18.5){
+        return "Underweight";
+    }
+    else if(bmi <= 25.0){
+        return "Normal";
+    }
+    else if(bmi <= 30.0){
+        return "Overweight";
+    }
+    else if(bmi > 30)
+        return "Obese";
+}
