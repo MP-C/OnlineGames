44abe229a32cac2194643e0115a05346f4cb0914
MP-C <mp.crcarvalho@gmail.com>
2023-11-12 09:30:13 +0100

    CodeWars_KeepHydrated.js

---
+++ JS/CodeWars_KeepHydrated.js
@@ -0,0 +1,17 @@
+function litres(time) {
+    return Math.floor(time*0.5);
+    //litres = Math.round((time*0.5))
+    //return litres - (litres %1)
+    //return Math.floor(litres)
+    //return Math.trunc(litres)
+    //return litres%2
+    //return Math.round(litres) 
+    //return parseInt(litres.toFixed(0))
+    //return parseInt(litres)
+    //return litres.toFixed(0);
+    /*
+    for(let i; i<litres.lenght; i++)
+        while (i!="."){
+    } return litres
+    */
+}
