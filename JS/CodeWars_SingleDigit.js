a1a19be2b67cfa755993bd755052359b000f8ce7
Mario CAPELA RODRIGUES CARVALHO <Mario CAPELA RODRIGUES CARVALHO>
2023-11-15 14:08:57 +0100

    CodeWars_SingleDigit.js

--- JS/CodeWars_SingleDigit.js
+++ JS/CodeWars_SingleDigit.js
@@ -1,8 +1,12 @@
 function singleDigit(n) {
+    if (n <= 9){
+        return n;    
+    }
+    return singleDigit(n.toString(2).split('').map(Number).reduce((accumulator, value) => accumulator += value));
+}
     /*
     let bin = [Number(n.toString(2))];
         let res = 0;
-
         for (let i = 0; i <= bin.length; i++){
         res += bin[i];
         console.log(bin[i]);
@@ -11,8 +15,3 @@ function singleDigit(n) {
         res += value;
     })
     */
-    if (n <= 9){
-        return n;    
-    }
-    return singleDigit(n.toString(2).split('').map(Number).reduce((accumulator, value) => accumulator += value));
-}
