f8cb0deec1cd29893b97f5272455325de979d901
MP-C <mp.crcarvalho@gmail.com>
2023-11-17 16:48:10 +0100

    Weather Observation Station 12

--- SQL/HR_SQL_Basic.sql
+++ SQL/HR_SQL_Basic.sql
@@ -230,3 +230,21 @@ Baldwin
 Barrigada 
 Bass Harbor
 */
+
+/*********************************************************************
+17) Weather Observation Station 12 
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY FROM STATION WHERE (CITY NOT IN (SELECT CITY FROM STATION WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%')) AND (CITY NOT IN (SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u'));
+/* OUTPUT
+Baker 
+Baldwin 
+Bass Harbor 
+Beaufort 
+Beaver Island 
+Benedict 
+...
+Yazoo City 
+Yoder 
+Zachary
+*/
