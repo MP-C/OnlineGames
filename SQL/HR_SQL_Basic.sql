c75c7c82d7c4e051d334677713b8e4df341e1bc7
MP-C <mp.crcarvalho@gmail.com>
2023-11-29 18:09:24 +0100

    Weather Observation Station 13

--- SQL/HR_SQL_Basic.sql
+++ SQL/HR_SQL_Basic.sql
@@ -507,3 +507,13 @@ SELECT SUBSTR(ROUND(SUM(LAT_N),2), 1, 8), SUBSTR(ROUND(SUM(LONG_W), 2), 1, 8) FR
 /* OUTPUT */
 42850.04 47381.48
 /*Not Working*/
+
+
+/*********************************************************************
+37) Weather Observation Station 13 
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880  and less than 137.2345.
+Truncate your answer to 4 decimal places.*/
+SELECT SUBSTR(ROUND(SUM(LAT_N),4), 1, 10) FROM STATION WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
+/* OUTPUT */
+36354.8135 
+/*Not Working*/
