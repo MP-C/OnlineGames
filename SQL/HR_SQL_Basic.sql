3991683c70e505211df242fb64846e858be1baff
MP-C <mp.crcarvalho@gmail.com>
2023-12-03 10:36:13 +0100

    Advanced SelectType of Triangle

--- SQL/HR_SQL_Basic.sql
+++ SQL/HR_SQL_Basic.sql
@@ -571,3 +571,50 @@ Europe 175139
 Oceania 109190
 South America 147436
 Africa 274439
+
+
+/*********************************************************************
+40) Advanced SelectType of Triangle
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+Equilateral: It's a triangle with 3 sides of equal length.
+Isosceles: It's a triangle with 2 sides of equal length.
+Scalene: It's a triangle with 1 sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle. */
+SELECT 
+CASE 
+WHEN A+B>C AND A+C>B AND B+C>A THEN 
+ CASE 
+   WHEN A=B AND B=C THEN 'Equilateral' 
+   WHEN A=B OR B=C OR A=C THEN 'Isosceles' 
+   ELSE 'Scalene'
+ END
+ ELSE 'Not A Triangle'
+END FROM TRIANGLES;
+
+/* OUTPUT */
+Equilateral 
+Equilateral 
+Isosceles 
+Equilateral 
+Isosceles 
+Equilateral 
+Scalene 
+Not A Triangle 
+Scalene 
+Scalene 
+Scalene 
+Not A Triangle
+
+/*Not Working*/
+SELECT 
+CASE 
+WHEN A+B>C AND A+C>B AND B+C>A THEN 
+ CASE 
+   WHEN A=B AND B=C THEN 'Equilateral' 
+   WHEN A=B OR B=C THEN 'Isosceles' 
+   ELSE 'Scalene'
+ END
+ ELSE 'Not A Triangle'
+END FROM TRIANGLES;
+
