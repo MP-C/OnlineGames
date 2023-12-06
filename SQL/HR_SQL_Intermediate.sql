e0041817f9ccd721ab44df85a8099426797a1eb5
capelam <mario.capela@stpierre-bru.be>
2023-12-07 00:45:10 +0100

    Alternative QueriesPrint Prime Numbers

--- SQL/HR_SQL_Intermediate.sql
+++ SQL/HR_SQL_Intermediate.sql
@@ -62,8 +62,6 @@ C2 Frank 45 45 45 45
 
 
 
-
-
 /*********************************************************************
 44)  Alternative QueriesPrint Prime Numbers
 Write a query to print all prime numbers less than or equal to 1000. Print your result on a single line, and use the ampersand (&) character as your separator (instead of a space).
