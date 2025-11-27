33110eae4315663eaf004836abfc88f39fdb8783
MP-C <mp.crcarvalho@gmail.com>
2025-11-27 22:10:34 +0100

    Tokyo_code_sheets.py

---
+++ Python/Tokyo_code_sheets.py
@@ -0,0 +1,15 @@
+#1 Completar o exercício aqui
+valor1 = float(input("Introduz um numero: "))
+valor2 = float(input("Introduz outro numero: "))
+
+# 1. Se os dois números são iguais (valor1 == valor2)
+print(f"{valor1 == valor2}")
+
+# 2. Se os dois números são diferentes (valor1 != valor2)
+print(f"{valor1 != valor2}")
+
+# 3. Se o primeiro é maior que o segundo (valor1 > valor2)
+print(f"{valor1 > valor2}")
+
+# 4. Se o primeiro é menor que o segundo (valor1 < valor2)
+print(f"{valor1 < valor2}")
