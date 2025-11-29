d094ace455e02211533ae4382fdfb646b682abd4
MP-C <mp.crcarvalho@gmail.com>
2025-11-29 18:00:14 +0100

    Tokyo_code_sheets.py

--- Python/Tokyo_code_sheets.py
+++ Python/Tokyo_code_sheets.py
@@ -13,3 +13,52 @@ print(f"{valor1 > valor2}")
 
 # 4. Se o primeiro é menor que o segundo (valor1 < valor2)
 print(f"{valor1 < valor2}")
+
+
+# Completar o exercício aqui
+#1
+lista = ["monte", "estadio", "pista", "estrada", "estrada"]
+tupla = ("monte", "estadio", "pista", "estrada", "estrada")
+
+#2 
+print("Isto é uma lista :", lista)
+print("Isto é uma tupla :", tupla)
+
+#3
+print("2º elemento da lista: ", lista[1])
+print("penultimo elemento da tupla: ", tupla[-2])
+
+#4
+lista[1]="novo terreno"
+
+tupla_transformada = list(tupla)
+tupla_transformada[2] = ("lago")
+tupla = tuple(tupla_transformada)
+
+#5
+print("Isto é lista com 'novo terreno': ", lista)
+print("Isto é tuple com 'lago': ", tupla)
+
+#6
+print("tamanho da list:", len(lista))
+print("tamanho de tuple:", len(tupla))
+
+#7
+lista.append("estadio")
+
+tupla_transformada = list(tupla)
+tupla_transformada.append("pista")
+tupla = tuple(tupla_transformada)
+
+print("Lista com 'estádio' : ", lista)
+print("Tuple com 'pista' :", tupla)
+
+#8
+lista.clear()
+
+tupla_transformada = list(tupla)
+tupla_transformada.clear()
+tupla = tuple(tupla_transformada)
+
+print("Isto é lista vazia: ", lista)
+print("Isto é tuple vazio: ", tupla)
