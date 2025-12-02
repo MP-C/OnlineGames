20e3d6094cdd204e10dffabb71277dc7eb9ff0f7
MP-C <mp.crcarvalho@gmail.com>
2025-12-03 00:19:27 +0100

    Tokyo_code_sheets.py

--- Python/Tokyo_code_sheets.py
+++ Python/Tokyo_code_sheets.py
@@ -1,4 +1,4 @@
-#1 Completar o exercício aqui
+### 1 Completar o exercício aqui
 valor1 = float(input("Introduz um numero: "))
 valor2 = float(input("Introduz outro numero: "))
 
@@ -15,7 +15,7 @@ print(f"{valor1 > valor2}")
 print(f"{valor1 < valor2}")
 
 
-# Completar o exercício aqui
+### 2 Completar o exercício aqui
 #1
 lista = ["monte", "estadio", "pista", "estrada", "estrada"]
 tupla = ("monte", "estadio", "pista", "estrada", "estrada")
@@ -62,3 +62,50 @@ tupla = tuple(tupla_transformada)
 
 print("Isto é lista vazia: ", lista)
 print("Isto é tuple vazio: ", tupla)
+
+
+### 3  Completar o exercício aqui
+#1
+lista1 = ["monte", "estadio", "pista", "estrada", "estrada"]
+um_set = set(lista1)
+
+#2
+dicionario = {"material": "pedómetro", "hidratação": "agua", "calçado":"sapatilhas","material": "pedómetro"}
+
+#3
+print("Isto é set: ", um_set)
+print("Isto é dicionario: ", dicionario)
+
+#4
+
+dic = list(dicionario.keys())
+print(dic[2])
+print("Isto é dicionario sem elemento 3: ", dic)
+
+#5 
+lista1 = list(um_set)
+lista1[1] = "olimpico"
+print("Isto é set sem 'olimpico': ", set(lista1))
+
+#6
+print(len(um_set))
+print(len(dicionario))
+
+#7
+print("estadio" in um_set)
+print("material" in dicionario)
+
+
+#8
+um_set.add("rampa")
+print("um_set: ",um_set)
+
+dicionario["temporisador"] = "Garmin"
+print("dicionario: ", dicionario)
+
+#9
+um_set.remove("pista")
+dicionario.pop("material")
+
+print("um_set: ",um_set)
+print("dicionario: ", dicionario)
