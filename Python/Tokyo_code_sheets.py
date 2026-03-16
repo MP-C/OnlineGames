872cb2ae29e59c8a9a89ed83b543a66f61a298f8
MP-C <mp.crcarvalho@gmail.com>
2026-03-16 19:51:22 +0100

    Tokyo_code_sheets.py Ex 41

--- Python/Tokyo_code_sheets.py
+++ Python/Tokyo_code_sheets.py
@@ -1847,3 +1847,42 @@ nome_ficheiro ="usuarios.csv"
 dicionario = carregar_dicionario(json_pessoas)
 converter_para_csv(dicionario, nome_ficheiro)
 imprimir(nome_ficheiro)
+
+
+
+########### 41
+import json
+
+def transformar_em_dicionario(lista):
+    print(lista)
+    lista_dictionary = json.loads(lista)
+    # print(lista_dictionary) # Resultado para testes
+    return lista_dictionary
+
+def guardar_ficheiro(dicionario, ficheiro_nome):
+    with open(ficheiro_nome, "w", encoding="utf-8") as ficheiro:
+        json.dump(dicionario, ficheiro, indent=4, ensure_ascii=False) # Transformar de python para JSON, Gravando o objeto diretamente no ficheiro
+
+def ler_ficheiro(ficheiro_nome):
+    with open(ficheiro_nome, "r", encoding="utf-8") as ficheiro: # Lê o conteúdo do ficheiro e Transformar em dicionário
+        dados = json.load(ficheiro) 
+        
+        frutas = dados["Frutaria"]["Frutas"]
+        print("\nQuantidades de frutas:")
+        for fruta in frutas:
+            print(f"{fruta['Nome']}: {fruta['Quantidade']}")
+
+        legumes = dados["Frutaria"]["Legumes"]
+        print("\nQuantidades de legumes:")
+        for legume in legumes:
+            print(f"{legume['Nome']}: {legume['Quantidade']}")
+
+mercearia = '{"Frutaria": { "Frutas": [{"Nome":"Bananas", "Quantidade":5}, {"Nome":"Mangas", "Quantidade":10}, {"Nome":"Maracuja", "Quantidade":6}], "Legumes": [{"Nome":"Batata Doce", "Quantidade":10}, {"Nome":"Corgete", "Quantidade":2}, {"Nome":"Beterraba", "Quantidade":4}]}}'
+ficheiro_nome = "frutaria.json"
+# print("Mercearia",type(mercearia)) #class 'str' => OK
+
+dicionario = transformar_em_dicionario(mercearia)
+# print(type(dicionario))  #class 'dict' => OK
+
+guardar_ficheiro(dicionario, ficheiro_nome)
+ler_ficheiro(ficheiro_nome)
